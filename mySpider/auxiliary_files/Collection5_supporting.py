21eb89d33cc6e9d92e2bade16505ad37bf152c6f
ana <61350652+humblegod-ana@users.noreply.github.com>
2021-04-30 19:19:49 +0800

    增加了个辅助文件夹

---
+++ mySpider/auxiliary_files/Collection5_supporting.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/30 19:10 
+# @Author  : ana
+# @File    : Collection6_supporting.py
+# @Software: PyCharm
+
+class Collection6Supporting:
+    startUrl = ['http://www.capitalmuseum.org.cn/jpdc/qtq.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/qtq_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/qtq_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/qtq_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/qtq_5.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/tcq.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/tcq_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/tcq_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/tcq_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/tcq_5.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/fzx.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/fzx_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/fzx_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/fzx_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/yq.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/yq_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/yq_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/yq_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/jyq.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/jyq_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/qb.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/sf.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/sf_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/sf_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/hh.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/hh_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/hh_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/xz.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/xz_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/xz_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/xz_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/xz_5.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/ww.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/ww_2.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/ww_3.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/ww_4.htm',
+                'http://www.capitalmuseum.org.cn/jpdc/ww_5.htm',
+                ]
