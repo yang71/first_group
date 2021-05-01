ad246be08214cff6d6906118ff5d87af8e6015be
ana <61350652+humblegod-ana@users.noreply.github.com>
2021-05-01 13:19:48 +0800

    更新了Collection15

---
+++ mySpider/auxiliary_files/Collection15_supporting.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/1 13:17 
+# @Author  : ana
+# @File    : Collection15_supporting.py
+# @Software: PyCharm
+
+
+class Collection15Supporting:
+    startUrl = ['http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx1.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx2.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx3.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx4.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx5.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx6.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx7.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx8.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx9.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx10.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx11.html?/ZLJS/CSZL/',
+                'http://www.automuseum.org.cn/ZLJS/CPJX/list-cpjx12.html?/ZLJS/CSZL/', ]
