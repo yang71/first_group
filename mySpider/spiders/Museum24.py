6fa9ccb7db10b46d2becc5982bafe313caa818cc
yang <1300978939@qq.com>
2021-05-18 14:34:35 +0800

    5/18小组讨论更新

--- mySpider/spiders/Museum24.py
+++ mySpider/spiders/Museum24.py
@@ -1,6 +1,7 @@
 import scrapy as scrapy
 
 from ..items import *
+from ..str_filter import *
 
 class Museum24(scrapy.Spider):
     name = "Museum24"
@@ -20,8 +21,8 @@ class Museum24(scrapy.Spider):
         item = MuseumBasicInformationItem()
         item["museumID"] = 24
         item["museumName"] = response.xpath('/html/head/title').xpath("string(.)").extract_first()
-        item["address"] = response.xpath('/html/body/div[1]/div/footer/div/div[2]/div/div[3]').extract_first()
-        item["openingTime"] = response.xpath("/html/body/div[1]/div/section[5]/div/div/div[2]/div/div[2]/div[2]").xpath("string(.)").extract_first()
+        item["address"] = response.xpath('/html/body/div[1]/div/footer/div/div[2]/div/div[3]').xpath("string(.)").extract_first()
+        item["openingTime"] = StrFilter.filter(response.xpath("/html/body/div[1]/div/section[5]/div/div/div[2]/div/div[2]/div[2]").xpath("string(.)").extract_first())
         item["consultationTelephone"] = "0310--3012739"
         item["introduction"] = "邯郸市博物馆现为国家一级博物馆、河北省爱国主义教育基地、第一批河北省社会科学普及基地和河北省志愿者实践基地。邯郸市博物馆成立于1984年，前身为成立于1968年的“毛泽东思想胜利万岁邯郸展览馆”。"
         item["publicityVideoLink"] = None
